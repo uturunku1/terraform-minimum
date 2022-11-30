7541a439e3543381dac204ae3217717eacaafb1c
uturunku1 <luces.huayhuaca@gmail.com>
2022-11-29 19:44:43 -0800

    remove error during plan

--- remote.tf
+++ remote.tf
@@ -126,33 +126,33 @@ variable "name_length" {
   default = 4
 }
 
-resource "random_pet" "always_error" {
-  lifecycle {
-    precondition {
-      condition     = var.name_length > 5 // false triggers error
-      error_message = "Name length must be greater than 5."
-    }
+# resource "random_pet" "always_error" {
+#   lifecycle {
+#     precondition {
+#       condition     = var.name_length > 5 // false triggers error
+#       error_message = "Name length must be greater than 5."
+#     }
 
-    postcondition {
-      condition     = var.name_length > 10 // false triggers error
-      error_message = "Name length must be greater than 5."
-    }
-  }
+#     postcondition {
+#       condition     = var.name_length > 10 // false triggers error
+#       error_message = "Name length must be greater than 5."
+#     }
+#   }
 
-  keepers = {
-    uuid = uuid() # Force a new name each time
-  }
-  length = var.name_length
-}
+#   keepers = {
+#     uuid = uuid() # Force a new name each time
+#   }
+#   length = var.name_length
+# }
+
+# output "pet" {
+#   value = random_pet.always_error.id
+# }
 resource "random_pet" "pet" {
   prefix = "{"
 }
 output "fail-at-apply" {
   value = jsondecode(random_pet.pet.id)
-}
-
-output "pet" {
-  value = random_pet.always_error.id
 }
 
 # locals {
