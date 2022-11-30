880a1c7d4768967c94ecd30adf166c4f7184c962
uturunku1 <luces.huayhuaca@gmail.com>
2022-11-30 11:48:16 -0800

    add error back

--- remote.tf
+++ remote.tf
@@ -143,15 +143,15 @@ variable "name_length" {
 #   length = var.name_length
 # }
 
-# output "pet" {
-#   value = random_pet.always_error.id
-# }
-# resource "random_pet" "pet" {
-#   prefix = "{"
-# }
-# output "fail-at-apply" {
-#   value = jsondecode(random_pet.pet.id)
-# }
+output "pet" {
+  value = random_pet.always_error.id
+}
+resource "random_pet" "pet" {
+  prefix = "{"
+}
+output "fail-at-apply" {
+  value = jsondecode(random_pet.pet.id)
+}
 
 # locals {
 #   a = join(["foo"], "bar")
