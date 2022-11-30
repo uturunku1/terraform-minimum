3a4fa30ab17fc4f726ef03d3f84a0a7f39690e18
uturunku1 <luces.huayhuaca@gmail.com>
2022-11-29 19:24:16 -0800

    push errors to main

--- remote.tf
+++ remote.tf
@@ -122,36 +122,36 @@ output "random_resource_always_new_name_id" {
 # }
 
 
-# variable "name_length" {
-#   default = 4
-# }
+variable "name_length" {
+  default = 4
+}
 
-# resource "random_pet" "always_error" {
-#   lifecycle {
-#     precondition {
-#       condition     = var.name_length > 5 // false triggers error
-#       error_message = "Name length must be greater than 5."
-#     }
+resource "random_pet" "always_error" {
+  lifecycle {
+    precondition {
+      condition     = var.name_length > 5 // false triggers error
+      error_message = "Name length must be greater than 5."
+    }
 
-#     postcondition {
-#       condition     = var.name_length > 10 // false triggers error
-#       error_message = "Name length must be greater than 5."
-#     }
-#   }
+    postcondition {
+      condition     = var.name_length > 10 // false triggers error
+      error_message = "Name length must be greater than 5."
+    }
+  }
 
-#   keepers = {
-#     uuid = uuid() # Force a new name each time
-#   }
-#   length = var.name_length
-# }
+  keepers = {
+    uuid = uuid() # Force a new name each time
+  }
+  length = var.name_length
+}
 
-# output "pet" {
-#   value = random_pet.always_error.id
-# }
+output "pet" {
+  value = random_pet.always_error.id
+}
 
-# locals {
-#   a = join(["foo"], "bar")
-#   b = max([1, 2, 3])
-#   c = abs("minus five")
-#   d = jsondecode("[1, 2, 3")
-# }
+locals {
+  a = join(["foo"], "bar")
+  b = max([1, 2, 3])
+  c = abs("minus five")
+  d = jsondecode("[1, 2, 3")
+}
